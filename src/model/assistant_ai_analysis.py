3eb5e44eaa938a0c0813fb00415be15dc82bf95c
Bruno Durante <37076921+bdurantec@users.noreply.github.com>
2025-02-12 11:04:50 -0300

    Delete src/model directory

--- src/model/assistant_ai_analysis.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class AssistantAIAnalysis:
-    has_sharp_object: bool
-    details: str = 'sharp object not found'
-    object_type: str = 'not_found'
--- src/model/image_analysis_result.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class ImageAnalysisResult:
-    details: str
-    object_type: str
-    image_stream: bytes
